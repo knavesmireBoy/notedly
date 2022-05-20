452ae95b26a3e8d9210a40fecf59402869b58dbe
Andrew Sykes <andrewsykes@btinternet.com>
2022-05-20 18:16:08 +0100

    pagination

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../components/Button';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Navigation from '../components/Navigation';
@@ -26,13 +27,69 @@ import ReactMarkdown from 'react-markdown';
   } }
   `;
 
+  const DoButton = (o) => {
 
-  const Home=()=>{ // query hook
+    fetchMore({
+      variables: {
+      cursor: o.cursor
+    }, 
+    updateQuery: (previousResult, { fetchMoreResult }) => {
+      return {
+        noteFeed: {
+          cursor: fetchMoreResult.o.cursor,
+          hasNextPage: fetchMoreResult.o.hasNextPage, 
+          // combine the new results and the old
+          notes: [
+            ...previousResult.o.notes,
+            ...fetchMoreResult.o.notes
+          ],
+          __typename: 'noteFeed'
+        }
+      };
+    }
+  
+  });
+
+  }
+
+
+  const Home=()=> { // query hook
     const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
          // if the data is loading, display a loading message
     if (loading) return <p>Loading...</p>;
     // if there is an error fetching the data, display an error message
     if (error) return <p>Error!</p>;
          // if the data is successful, display the data in our UI
-    return <NoteFeed notes={data.noteFeed.notes} />; };
+    return (<React.Fragment>
+      <NoteFeed notes={data.noteFeed.notes} />
+{/* Only display the Load More button if hasNextPage is true */}
+{data.noteFeed.hasNextPage && ( <Button onClick={ (e) => 
+{
+  fetchMore({
+    variables: {
+    cursor: data.noteFeed.cursor
+  }, 
+  updateQuery: (previousResult, { fetchMoreResult }) => {
+    return {
+      noteFeed: {
+        cursor: fetchMoreResult.noteFeed.cursor,
+        hasNextPage: fetchMoreResult.noteFeed.hasNextPage, 
+        // combine the new results and the old
+        notes: [
+          ...previousResult.noteFeed.notes,
+          ...fetchMoreResult.noteFeed.notes
+        ],
+        __typename: 'noteFeed'
+      }
+    };
+  }
+
+});
+  }
+  }>Load more</Button> )}
+      </React.Fragment>
+      )
+  };
     export default Home;
+
+
