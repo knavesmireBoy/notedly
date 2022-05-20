4db6b76af350916f95f794e3a2b3ab0477545393
Andrew Sykes <andrewsykes@btinternet.com>
2022-05-20 15:57:05 +0100

    123

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Layout from '../components/Layout';
 import Home from './home';
 import MyNotes from './mynotes'; 
 import Favorites from './favorites';
+import NotePage from './note';
+
 
 const Pages = () => { return (
     <Router>
@@ -12,6 +14,7 @@ const Pages = () => { return (
       <Route exact path="/" component={Home} />
       <Route path="/mynotes" component={MyNotes} />
       <Route path="/favorites" component={Favorites} />
+      <Route path="/note/:id" component={NotePage} />
       </Layout>
 </Router> );
 };
---
+++ src/pages/note.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const NotePage = props => { return (
+        <div>
+          <p>ID: {props.match.params.id}</p>
+</div> );
+};
+export default NotePage;
