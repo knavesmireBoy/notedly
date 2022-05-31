56664b5b23a53613599f75a589c082b801412249
Andrew Sykes <andrewsykes@btinternet.com>
2022-05-31 07:31:42 +0100

    prepare new note

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 // import shared layout component
 import Layout from '../components/Layout';
 import Home from './home';
@@ -10,17 +10,53 @@ import NotePage from './note';
 import SignUp from './signup';
 import SignIn from './signin';
 
+    // import the NewNote route component
+    import NewNote from './new';
 
-const Pages = () => { return (
+import { useQuery, gql } from '@apollo/client';
+const IS_LOGGED_IN = gql` {
+        isLoggedIn @client
+      }
+`;
+
+
+
+   // add the PrivateRoute component below our `Pages` component
+   const PrivateRoute = ({ component: Component, ...rest }) => { 
+    const { loading, error, data } = useQuery(IS_LOGGED_IN);
+    // if the data is loading, display a loading message
+   if (loading) return <p>Loading...</p>;
+    // if there is an error fetching the data, display an error message
+   if (error) return <p>Error!</p>;
+   // if the user is logged in, route them to the requested component
+   // else redirect them to the sign-in page
+   return (
+           <Route
+             {...rest}
+             render={props =>
+   data.isLoggedIn === true ? ( <Component {...props} />
+   ):( <Redirect
+                   to={{
+                     pathname: '/signin',
+                     state: { from: props.location }
+   }}
+   />
+   ) }
+   />
+   ); };
+
+   const Pages = () => { return (
     <Router>
       <Layout>
       <Route exact path="/" component={Home} />
-      <Route path="/mynotes" component={MyNotes} />
-      <Route path="/favorites" component={Favorites} />
+      <PrivateRoute path="/mynotes" component={MyNotes} />
+      <PrivateRoute path="/favorites" component={Favorites} />
+      <PrivateRoute path="/new" component={NewNote} />
       <Route path="/note/:id" component={NotePage} />
       <Route path="/signup" component={SignUp} />
       <Route path="/signin" component={SignIn} />
       </Layout>
 </Router> );
 };
+
 export default Pages;
---
+++ src/pages/new.js
@@ -0,0 +1,11 @@
+
+import React, { useEffect } from 'react';
+import { useMutation, gql } from '@apollo/client';
+import NoteForm from '../components/NoteForm';
+const NewNote = props => { useEffect(() => {
+        // update the document title
+        document.title = 'New Note — Notedly';
+      });
+return <NoteForm />;
+};
+export default NewNote;
